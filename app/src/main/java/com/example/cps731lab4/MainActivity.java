34e612a4c5a91bf7405d378c66af61f1c9ac0c6d
seden <sakman@ryerson.ca>
2020-10-04 16:45:40 -0400

    working on main activity, displaying the list items

--- app/src/main/java/com/example/cps731lab4/MainActivity.java
+++ app/src/main/java/com/example/cps731lab4/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.cps731lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private ArrayList<String> mTitles = new ArrayList<>();
+    private ArrayList<String> mDescriptions = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initList();
+    }
+    private void initList(){
+
+        mTitles.add("Title 1");
+        mDescriptions.add("Desc 1");
+        mTitles.add("Title 2");
+        mDescriptions.add("Desc 2");
+        mTitles.add("Title 3");
+        mDescriptions.add("Desc 3");
+        mTitles.add("Title 4");
+        mDescriptions.add("Desc 4");
+        mTitles.add("Title 5");
+        mDescriptions.add("Desc 5");
+        mTitles.add("Title 6");
+        mDescriptions.add("Desc 6");
+
+        initRecyclerView();
+
+    }
+    private void initRecyclerView(){
+
+            RecyclerView recyclerView = findViewById(R.id.rvList);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            RecyclerViewAdapter adapter=new RecyclerViewAdapter(this,mTitles,mDescriptions);
+            recyclerView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/cps731lab4/RecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package com.example.cps731lab4;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private ArrayList<String> mTitles= new ArrayList<>();
+    private ArrayList<String> mDescriptions = new ArrayList<>();
+    private Context mcontext;
+
+    public RecyclerViewAdapter(Context context,ArrayList<String> titles, ArrayList<String> desc){
+        mTitles=titles;
+        mDescriptions=desc;
+        mcontext=context;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    holder.title.setText(mTitles.get(position));
+    holder.description.setText(mDescriptions.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView description;
+        LinearLayout parentLayout;
+        public ViewHolder(View itemView){
+            super(itemView);
+            title=itemView.findViewById(R.id.txtId);
+            description=itemView.findViewById(R.id.txtDescription);
+            parentLayout=itemView.findViewById(R.id.prLayout);
+
+        }
+
+    }
+}
