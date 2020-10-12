56199cdab38a381adf4bded048b656a380fc7067
Seden <sakman@ryerson.ca>
2020-10-11 22:11:54 -0400

    Sound effect has been added to splash

--- app/src/main/java/com/example/cps731lab4/Splash.java
+++ app/src/main/java/com/example/cps731lab4/Splash.java
@@ -3,21 +3,26 @@ package com.example.cps731lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
-
+    MediaPlayer mdDrums;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mdDrums=new MediaPlayer();
+        mdDrums=MediaPlayer.create(this,R.raw.drums);
+        mdDrums.start();
         TimerTask task=new TimerTask() {
             @Override
             public void run() {
                 finish();
+                mdDrums.pause();
                 startActivity(new Intent(Splash.this, HeroListActivity.class));
 
             }
